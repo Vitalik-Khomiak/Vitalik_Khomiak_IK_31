58db1e03f1ae86e5e20815219102a2467674a068
ArtLab <vitaliikhomiakk@gmail.com>
2021-10-23 19:32:21 +0300

    lab_2_if

--- lab_2/test.py
+++ lab_2/test.py
@@ -1,9 +1,19 @@
-
+#2.1
 print("First_constant: ", True)
 print("Thirs_constant: ", None)
 print("Second_constant: ", False)
 
+#2.2
 x = 2
 print(f"x + 2 = {eval('x+2')}")
 print(f"4 в квадраті = {pow(4,2)}")
 print("Максимальне число з чисел 5,8,9,3,45 :",max(5,8,9,3,45))
+
+#2.3
+x=10
+if x==10:
+    print ('x=10')
+
+else:
+    print ('x!=10')
+
