b0222102d4b49ab144033a63ecef8939d7742740
artlab <vitaliikhomiakk@gmail.com>
2021-11-14 13:34:05 +0200

    lab_3_fin_fin_fin

--- lab_3/my_app/urls.py
+++ lab_3/my_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', views.main, name='my_app'),
     path('health/', views.health, name='health')
 ]
